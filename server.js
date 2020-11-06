d21a5c183bb57a6066b0f4e52638adcebdb8700f
Jacob Esswein <essweinjacob@gmail.com>
2020-11-05 19:49:08 -0600

    UI and functionality added

---
+++ server.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utilities/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utilities/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const serverName = "Server";
+
+// Run for when client connects
+io.on('connection', socket => {
+    socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room)
+
+        // Welcome new users
+        socket.emit('message', formatMessage(serverName, 'Welcome to the Chat'));
+
+        // Notify when user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(serverName, `${user.username} has joined the chat`));
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    });
+
+    // Notify on user disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        
+        if(user){
+            io.to(user.room).emit('message', formatMessage(serverName, `${user.username} has left the chat`));
+        }
+
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log('Server running on port ' + PORT ));
+
